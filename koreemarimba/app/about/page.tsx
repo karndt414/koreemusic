84c205b62619a4b5469d88b455efaf1941b6ab6c
karndt414 <koree.arndt@gmail.com>
2026-02-17 12:48:58 -0600

    About updates

--- koreemarimba/app/about/page.tsx
+++ koreemarimba/app/about/page.tsx
@@ -17,6 +17,8 @@ export default function About() {
 
         <p>In addition, I performed with the Brownsburg Winter Percussion, where we were finalists in the WGI World Class 2024 with the show &quot;Curated.&quot;</p>
 
+        <p>Now, I live in Bentonville, Arkansas, where I continue to pursue my passion for music. I have again had the chance to perform at Grand National Finals with the show &quot;Sequ1nce&quot; in 2025. I've also composed and released my very first original works while here at Bentonville!</p>
+
         <h3>Achievements</h3>
         <ul>
           <li>ISSMA State Solos - Perfect Scores</li>
